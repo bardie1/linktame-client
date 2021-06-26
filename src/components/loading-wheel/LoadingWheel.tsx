67e91f22b46c2cbb2fcc310df089235fcffd862c
saul bard <saul@tecsico.com.au>
2021-06-27 01:58:18 +1000

    loading wheel component created

---
+++ src/components/loading-wheel/LoadingWheel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './LoadingWheel.css';
+export const LoadingWheel = () => {
+    return (
+        <div className="loading-wheel-container">
+            <div className="loading-wheel-loader">
+
+            </div>
+        </div>
+    )
+}
