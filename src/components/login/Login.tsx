d5560a468a1337747d84ef1de73c9520138c482c
saul bard <saul@tecsico.com.au>
2021-06-27 01:59:10 +1000

    add redux to login page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/slicers/userSlicer';
+import { sessionService } from '../../services/session.service';
 
 import './Login.css';
 
@@ -13,6 +16,7 @@ export const Login = ({ formSwitch }: LoginProps) => {
 
     const [formValid, setFormValid] = useState<boolean>(false);
 
+    const dispatch = useDispatch();
 
     useEffect(() => {
         console.log("this");
@@ -22,6 +26,12 @@ export const Login = ({ formSwitch }: LoginProps) => {
             setFormValid(false);
         }
     }, [email, password])
+
+    const loginToApp = (e: any) => {
+        e.preventDefault();
+        dispatch(login({email: email, password: password}));
+        sessionService.setUser({email: email, password: password});
+    }
 
     return (
         <div className="login-container">
@@ -40,7 +50,7 @@ export const Login = ({ formSwitch }: LoginProps) => {
                         <input onChange={e => setPassword(e.target.value)} type="password"/>
                     </div>
                     <div className="login-btn-holder">
-                        <button onClick={(e) => e.preventDefault() } disabled={!formValid} id="login-btn" className="filled" type="submit">Login</button>
+                        <button onClick={(e) => loginToApp(e) } disabled={!formValid} id="login-btn" className="filled" type="submit">Login</button>
                     </div>
                     <div className="already-account">
                         <p>Don't have an account with us? <span onClick={() => formSwitch(false)} className="login-clink">Sign Up</span></p>
